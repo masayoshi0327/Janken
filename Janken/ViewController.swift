7f303f597337745d40ad2182a480ea86ed9d0c95
masayoshi0327 <75524615+masayoshi0327@users.noreply.github.com>
2021-03-16 18:26:32 +0900

    ランダムでじゃんけん表示

--- Janken/ViewController.swift
+++ Janken/ViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var jankenNum = 0
+    let janken = ["なし", "グー", "チョキ", "パー"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +20,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var answerImageView: UIImageView!
     @IBOutlet weak var answerLabel: UILabel!
     @IBAction func shuffleAction(_ sender: Any) {
+        jankenNum = Int.random(in: 1..<4)
+        answerImageView.image = UIImage(named: janken[jankenNum])
     }
 }
 
